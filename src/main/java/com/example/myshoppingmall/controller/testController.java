f8a3e44eecb924fb28daff1593edd266bce972b5
SierraSon97 <swj1265@naver.com>
2022-07-23 17:46:58 +0900

    Initial commit

---
+++ src/main/java/com/example/myshoppingmall/controller/testController.java
@@ -0,0 +1,19 @@
+package com.example.myshoppingmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class testController {
+    @GetMapping("/main")
+    public String testMain(){
+        return "/layouts/mainLayout";
+    }
+
+    @GetMapping("/login")
+    public String testLogin(){
+        return "/member/memberLoginForm";
+    }
+}
---
+++ src/test/java/com/example/myshoppingmall/MyShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShoppingMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
